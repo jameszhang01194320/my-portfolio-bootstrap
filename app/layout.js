6ed3cea21ef9d530e4b851572e884af73b6f16d7
jameszhang01194320 <wuab@yahoo.com>
2025-08-07 00:38:17 -0700

    build: regenerate static site

--- app/layout.js
+++ app/layout.js
@@ -11,14 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <base href="/my-portfolio-bootstrap/" />
-
-        {/* ✅ Bootstrap CDN */}
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-          integrity="sha384-1CmrxMRARb6aLqgBO7WkRyWq6cS8e5csXv5P4eakv1I0vAAHavbY09x7M6D3Bdp1"
-          crossOrigin="anonymous"
-        />
+        <link rel="stylesheet" href="bootstrap.min.css" />
       </head>
       <body>{children}</body>
     </html>
