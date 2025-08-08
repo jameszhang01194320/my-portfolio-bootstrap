4112ac580d5915e54dafdf4b40e24439436a8292
jameszhang01194320 <wuab@yahoo.com>
2025-08-08 11:40:19 -0700

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,17 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 静态导出
   output: 'export',
+
+  // 禁用 Next.js 的图片优化，以支持静态导出
   images: { unoptimized: true },
-  basePath: isProd ? '/my-portfolio-bootstrap' : '',
-  assetPrefix: isProd ? '/my-portfolio-bootstrap/' : '',
+
+  // 将所有路径相关的配置删除或设置为空字符串
+  basePath: '',
+  assetPrefix: '',
   env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? '/my-portfolio-bootstrap' : '',
+    NEXT_PUBLIC_BASE_PATH: '',
   },
 };
 
