45dff41b0eafbec508fff6ae5e061929451ae545
jameszhang01194320 <wuab@yahoo.com>
2025-08-08 10:31:24 -0700

    feat: Add out directory for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,11 @@
-// next.config.mjs
 const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'docs',
-  basePath: isProd ? '/my-portfolio-bootstrap' : '',
   images: { unoptimized: true },
-
-  // 可选：局域网跨域警告去除（dev 时）
-  allowedDevOrigins: ['http://localhost:3000', 'http://192.168.0.3:3000'],
-
-  // 给组件里拼图片前缀用（线上：/my-portfolio-bootstrap，本地：''）
+  basePath: isProd ? '/my-portfolio-bootstrap' : '',
+  assetPrefix: isProd ? '/my-portfolio-bootstrap/' : '',
   env: {
     NEXT_PUBLIC_BASE_PATH: isProd ? '/my-portfolio-bootstrap' : '',
   },
